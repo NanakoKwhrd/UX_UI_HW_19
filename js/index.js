f0ddd6951e23b0fc5eaca6b171ce0690392dae25
NanakoKwhrd <nanako.kwhrd@gmail.com>
2021-08-29 10:57:47 -0400

    Js

---
+++ js/index.js
@@ -0,0 +1,78 @@
+
+
+
+  $( "#downloadResume" ).click(function() {
+    $( "#popup" ).show( "slow", function() {
+      
+    });
+  });
+
+
+  $( "#close" ).click(function() {
+    $( "#popup" ).hide( "slow", function() {
+      
+    });
+  });
+
+
+
+function fade() {
+  $('.preloader').fadeOut("slow");
+  }
+  setTimeout(fade, 3000);
+
+
+
+
+
+$(function() {
+  $('#click').hover(function(){
+      $(this).addClass("click");
+  },
+  function(){
+      $(this).removeClass('click');
+  });
+});  
+
+
+$(function() {
+  $('#click2').hover(function(){
+      $(this).addClass("click");
+  },
+  function(){
+      $(this).removeClass('click');
+  });
+});  
+
+
+$(function() {
+  $('#click3').hover(function(){
+      $(this).addClass("click");
+  },
+  function(){
+      $(this).removeClass('click');
+  });
+});  
+
+
+$(function() {
+  $('#click4').hover(function(){
+      $(this).addClass("click");
+  },
+  function(){
+      $(this).removeClass('click');
+  });
+});  
+
+
+$(function() {
+  $('#click5').hover(function(){
+      $(this).addClass("click");
+  },
+  function(){
+      $(this).removeClass('click');
+  });
+});  
+
+
+
